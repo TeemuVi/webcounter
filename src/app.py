2dd4d263c6d2636d6d45fda00438d647e06de519
Teemu Vi <teemu.vierros@gmail.com>
2025-11-17 21:56:40 +0200

    made some progress on the exercise

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect,request, render_template
 from counter import Counter
 
 app = Flask(__name__)
@@ -17,3 +17,19 @@ def increment():
 def reset():
     cnt.reset()
     return redirect("/")
+
+@app.route("/set", methods=["POST"])
+def set_number():
+    number = int(request.form.get("value",0))
+    cnt.value = number
+    return redirect("/")
+    
+@app.route("/reset", methods=["POST"])
+def reset():
+    cnt.value = 0
+    return redirect("/")
+    
+@app.route("/reset", methods=["POST"])
+def test_reset():
+    cnt.value = 0
+    return "Reset"
